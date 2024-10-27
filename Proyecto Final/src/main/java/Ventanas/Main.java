0fc33783b88af3253a4237df54ffbc1997d66833
Iren12sl <162946621+Iren12sl@users.noreply.github.com>
2024-10-27 00:30:50 -0600

    Add files via upload

---
+++ Proyecto Final/src/main/java/Ventanas/Main.java
@@ -0,0 +1,12 @@
+package Ventanas;
+
+public class Main {
+    public static void main(String[] args) {
+        VentanaPrincipal vl = new VentanaPrincipal();
+        vl.setVisible(true);
+
+
+    }
+
+
+}
